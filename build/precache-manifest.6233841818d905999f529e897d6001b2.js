a586c184c24965903a6e559313b3e3ff1dae663f
Artur-developer12 <61634745+Artur-developer12@users.noreply.github.com>
2020-03-21 13:52:36 +0300

    Добавление файла сборки

---
+++ build/precache-manifest.6233841818d905999f529e897d6001b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df2796e80208faa9128780dd43bfa78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499d50948e5ef0783596",
+    "url": "/static/js/2.7b21bbe7.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.7b21bbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2055eb21930e954fec9f",
+    "url": "/static/js/main.7122f74f.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
